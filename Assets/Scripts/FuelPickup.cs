c821c69e7d83d1313c94b1691bdce35dedbf896a
LCD-PC <lcd11001@gmail.com>
2025-08-21 19:46:30 +0700

    + Fuel pickup

--- Assets/Scripts/FuelPickup.cs
+++ Assets/Scripts/FuelPickup.cs
@@ -5,13 +5,16 @@ public class FuelPickup : MonoBehaviour
     [SerializeField]
     private float fuelAmount = 5f; // Amount of fuel to add when picked up
 
+    [Tooltip("Event channel to raise when fuel is picked up")]
+    [SerializeField]
+    private FuelEventChannelSO fuelEvent;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Lander lander = collision.GetComponent<Lander>();
-        if (lander != null)
+        if (fuelEvent != null)
         {
-            lander.AddFuel(fuelAmount);
-            Destroy(gameObject); // Destroy the pickup after collection
+            fuelEvent.RaiseEvent(fuelAmount);
         }
+        Destroy(gameObject); // Destroy the pickup after collection
     }
 }
