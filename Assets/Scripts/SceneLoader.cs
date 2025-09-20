da4bd9f06f8c6a276a943bc1ca99cd3f35b40ade
LCD-PC <lcd11001@gmail.com>
2025-09-20 09:04:46 +0700

    * error SceneAsset when building

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,12 +1,33 @@
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
     public static SceneLoader Instance { get; private set; }
+
+#if UNITY_EDITOR
     [SerializeField] private SceneAsset gameScene;
     [SerializeField] private SceneAsset mainMenuScene;
+#endif
+    [SerializeField, HideInInspector] private string gameSceneName;
+    [SerializeField, HideInInspector] private string mainMenuSceneName;
+
+    private void OnValidate()
+    {
+#if UNITY_EDITOR
+        if (gameScene != null)
+        {
+            gameSceneName = gameScene.name;
+        }
+        if (mainMenuScene != null)
+        {
+            mainMenuSceneName = mainMenuScene.name;
+        }
+#endif
+    }
 
     private void Awake()
     {
@@ -21,18 +42,18 @@ public class SceneLoader : MonoBehaviour
 
     public AsyncOperation LoadGameScene()
     {
-        if (gameScene != null)
+        if (!string.IsNullOrEmpty(gameSceneName))
         {
-            return SceneManager.LoadSceneAsync(gameScene.name);
+            return SceneManager.LoadSceneAsync(gameSceneName);
         }
         return null;
     }
 
     public AsyncOperation LoadMainMenuScene()
     {
-        if (mainMenuScene != null)
+        if (!string.IsNullOrEmpty(mainMenuSceneName))
         {
-            return SceneManager.LoadSceneAsync(mainMenuScene.name);
+            return SceneManager.LoadSceneAsync(mainMenuSceneName);
         }
         return null;
     }
